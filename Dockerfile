6077bf26c6decf1ceb173ad8bada38da0b861f18
Alexey Kachanov <35109763+lexeyOK@users.noreply.github.com>
2021-12-31 10:44:44 +0500

    initiate db in docker

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,9 @@ RUN pip install -r requirements.txt
 EXPOSE 5000
 
 COPY . .
+
+RUN flask db init
+RUN flask db migrate -m "Initial migration"
+RUN flask db upgrade
+
 CMD ["flask", "run"]
--- init.sh
+++ init.sh
@@ -1,10 +1,18 @@
 git clone https://github.com/diduk001/math-tournir-72.git
 cd math-tournir-72/
+
 python3 --version
 pip install virtualenv
 pip install virtualenvwrapper
 mkvirtualenv -a $(pwd) -r requirements.txt math-tournir-72
+
 pip install -r  requirements.txt
+
+flask db init
+flask db migrate -m "Initial migration"
+flask db upgrade
+
+flask run
 
 deactivate
 lsvirtualenv
